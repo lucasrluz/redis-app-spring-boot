38ec79addf8e6e6f4b9071aed937acafe470f77e
lucasrÃluz <lucasr.luz42@gmail.com>
2024-01-18 15:13:25 -0300

    feat: primeiro commit

---
+++ src/main/java/com/redisappspringboot/RedisAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.redisappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisAppSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redisappspringboot/RedisAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redisappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
